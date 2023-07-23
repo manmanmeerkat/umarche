94480df48c6ac3ac04135b8a0fa1bbbc040986a0
manmanmeer <masak6161@gmail.com>
2023-07-23 15:45:35 +0900

    image, create, validation

--- app/Http/Requests/UploadImageRequest.php
+++ app/Http/Requests/UploadImageRequest.php
@@ -22,7 +22,8 @@ class UploadImageRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'image'=>'image|mimes:jpg,jpeg,png|max:2048',
+            'image' => 'image|mimes:jpg,jpeg,png|max:2048',
+            'files.*.image' => 'required|image|mimes:jpg,jpeg,png|max:2048'
         ];
     }
 
